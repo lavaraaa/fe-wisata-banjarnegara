9afdd7d8b84f80f4aee7f6701e91d8dbc89ac6bb
Latief <latifftrahman@gmail.com>
2026-01-03 13:18:37 +0700

    fix

--- src/components/chatbotComponents/NavbarChatbot.jsx
+++ src/components/chatbotComponents/NavbarChatbot.jsx
@@ -21,21 +21,8 @@ const NavbarChatbot = () => {
           zIndex: 1300,
         }}
       >
-        {user && (
-          <span
-            style={{ cursor: 'pointer', fontSize: '20px' }}
-            onClick={() => setSidebarOpen(true)}
-          >
-            ☰
-          </span>
-        )}
         <p style={{ margin: 0, marginRight: '70%' }}>Chatbot</p>
-     
       </div>
-
-      {sidebarOpen && user && (
-        <SidebarChatbot onClose={() => setSidebarOpen(false)} />
-      )}
     </>
   );
 };
